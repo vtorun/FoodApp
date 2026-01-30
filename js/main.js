f7f0a32f27762474a6c6d5b5422350e780793a12
vtorun <volkantorunn@hotmail.com>
2026-01-30 01:32:29 +0100

    end

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ async function loadMenu() {
   try {
     const menuData = await getMenu();
     renderMenu(menuData);
+    setupCategoryFilter(menuData);
   } catch (error) {
     console.error("Menu loading failed:", error);
   }
